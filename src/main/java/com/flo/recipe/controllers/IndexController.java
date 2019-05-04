41c74e8920a27eff1d5783828fba992ce281d230
NEO <neo.example.com>
2019-05-04 22:10:36 +0200

    Added Repository and Controller code

--- src/main/java/com/flo/recipe/controllers/IndexController.java
+++ src/main/java/com/flo/recipe/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package com.flo.recipe.controllers;
 
+import com.flo.recipe.domain.Category;
+import com.flo.recipe.domain.UnitOfMeasure;
+import com.flo.recipe.repositories.CategoryRepository;
+import com.flo.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("Some message to say... 123455555eeeee");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/flo/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/flo/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.flo.recipe.repositories;
 import com.flo.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
